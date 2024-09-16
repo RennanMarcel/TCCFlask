d20c0964ce6fd7fe871b50c331003f739623d75f
RennanMarcel <168394033+RennanMarcel@users.noreply.github.com>
2024-09-16 12:02:02 -0300

    Add files via upload

--- TCC/app.py
+++ TCC/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'  # Necessário para usar sessões
 
 @app.route('/')
 def index():
@@ -8,20 +9,34 @@ def index():
 
 @app.route('/agendamentos')
 def agendamentos():
+    if 'loginStatus' not in session:
+        return redirect(url_for('login'))
     return render_template('agendamentos.html')
 
 @app.route('/fale_conosco')
 def fale_conosco():
-        return render_template('contate-nos.html')
-
+    return render_template('contate-nos.html')
 
 @app.route('/eventos')
 def eventos():
     return render_template('eventos.html')
 
-@app.route('/adm')
-def adm():
-        return render_template('adm.html')
+@app.route('/instagram')
+def instagram():
+    return redirect('https://www.instagram.com/cscjuniorletal')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form.get('nome')
+        senha = request.form.get('senha')
+        
+        if login == "CSCJL1" and senha == "cscjl":
+            session['loginStatus'] = True
+            return redirect(url_for('eventos'))
+        else:
+            return redirect(url_for('login'))
+    return render_template('adm.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
